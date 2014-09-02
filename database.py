451d0f21c4329c8d02009e8711d328d230c7123d
Enrico Lamperti <mail@elamperti.com>
2014-09-01 22:54:55 -0300

    Better method names. Added get_tags() method and a placeholder for add_tags()

--- database.py
+++ database.py
@@ -13,15 +13,15 @@ class BastardSQL():
         self.cursor.executescript(sql)
         pass
 
-    def getConversations(self):
+    def get_conversations(self):
         """Returns an iterable list of conversations"""
         return self.cursor.execute("SELECT * FROM [Conversations]")
 
-    def getConversation(self, conv_id):
+    def get_conversation(self, conv_id):
         """Returns a single conversation if found"""
         return self.cursor.execute("SELECT * FROM [Conversations] WHERE [conv_id] = ?", conv_id)
 
-    def putConversation(self, conv_id, conv_name, participants):
+    def put_conversation(self, conv_id, conv_name, participants):
         """Inserts a conversation in the Conversations table, returns the conv_id"""
         self.cursor.execute("INSERT INTO [Conversations] ([conv_id], [conv_name], [participants]) VALUES (?, ?, ?)", (
                 conv_id,
@@ -30,14 +30,14 @@ class BastardSQL():
             ))
         return self.cursor.lastrowid
 
-    def getMessages(self, conv_id=0):
+    def get_messages(self, conv_id=0):
         """Returns an iterable list of all messages (or the messages on a conversation if conv_id is defined)"""
         if conv_id > 0:
             return self.cursor.execute("SELECT * FROM [Messages] WHERE msg_conv_id = {} ORDER BY [msg_timestamp] DESC".format(conv_id))
         else:
             return self.cursor.execute("SELECT * FROM [Messages] ORDER BY [msg_timestamp] DESC".format(conv_id))
 
-    def putMessage(self, conv_id, message, author, timestamp):
+    def put_message(self, conv_id, message, author, timestamp):
         """Inserts a message in the Messages table, returns the row ID"""
         self.cursor.execute("INSERT INTO [Messages] ([msg_conv_id], [msg_content], [msg_author_id], [msg_timestamp]) VALUES (?, ?, ?, ?)", (
                 conv_id,
@@ -46,6 +46,15 @@ class BastardSQL():
                 timestamp
             ))
         return self.cursor.lastrowid
+
+    def add_tags(self, msg_id, author, tags):
+        """Adds tags to a message"""
+        pass
+
+    def get_tags(self, msg_id, author=0):
+        """Returns all the tags for a given message"""
+        result = self.cursor.execute("SELECT [msg_tags] FROM [Messages] WHERE msg_id = {}".format(msg_id))
+        return result.fetchone()[0]
 
 if __name__ == '__main__':
     print("Populating database...")
