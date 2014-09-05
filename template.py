85a538bdc65c2918e6519ec2c012557395332836
Enrico Lamperti <mail@elamperti.com>
2014-09-05 01:35:06 -0300

    Template decorator for CherryPy using Jinja2. Base layout using Bootstrap and views.

---
+++ template.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+
+import os
+import datetime
+
+import cherrypy
+from jinja2 import Environment, FileSystemLoader, TemplateNotFound
+
+
+def template(filename=None):  # , *args, **kwargs
+    def wrap(f, *args, **kwargs):
+        def wrapped_f(*args, **kwargs):
+            params = f(*args, **kwargs)
+            jinja = Environment(
+                loader=FileSystemLoader('templates'),
+                extensions=['jinja2.ext.with_']
+            )
+
+            if not filename:
+                raise cherrypy.HTTPError(500, "Template not defined")
+            else:
+                filenames = [filename + '.tlp']  # ..scope royals vs local bastards
+
+            if '__template' in params:
+                if isinstance(params['__template'], basestring):
+                    filenames.insert(0, params['__template'] + '.tlp')
+                else:
+                    # Don't use +=, we are leaving it *after* __template for precedence
+                    filenames = [s + '.tlp' for s in params['__template']] + filenames
+
+            try:
+                t = jinja.select_template(filenames)
+            except TemplateNotFound:
+                raise cherrypy.HTTPError(404, "Template not found")
+
+            t.globals['timestamp'] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            #t.globals['session'] = cherrypy.request.session
+
+            return t.render(**params)
+
+        return wrapped_f
+
+    return wrap
