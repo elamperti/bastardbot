07ddaa955633bddce699ec315f22045c6bd8fe07
Enrico Lamperti <mail@elamperti.com>
2014-09-07 22:45:06 +0000

    Message types and authors

--- bastard.sql
+++ bastard.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS [Conversations];
 CREATE TABLE [Conversations]
 (
-    [conv_id] NVARCHAR(150) NOT NULL,
+    [conv_id] VARCHAR(26) NOT NULL,
     [conv_name] NVARCHAR(140),
     [participants] TEXT,
     CONSTRAINT [PK_conv] PRIMARY KEY ([conv_id])
@@ -11,19 +11,45 @@ DROP TABLE IF EXISTS [Messages];
 CREATE TABLE [Messages]
 (
     [msg_id] INTEGER,
-    [msg_conv_id] NVARCHAR(150) NOT NULL,
+    [msg_conv_id] VARCHAR(26) NOT NULL,
     [msg_content] TEXT,
     [msg_author_id] INTEGER,
     [msg_timestamp] DATETIME,
+    [msg_type] INTEGER DEFAULT 1,
     [msg_tags] TEXT,
     CONSTRAINT [PK_msg] PRIMARY KEY ([msg_id]),
     FOREIGN KEY ([msg_conv_id]) REFERENCES [Conversations] ([conv_id])
+        ON DELETE NO ACTION ON UPDATE NO ACTION,
+    FOREIGN KEY ([msg_type]) REFERENCES [Message_Types] ([type_id])
         ON DELETE NO ACTION ON UPDATE NO ACTION
 );
 
+DROP TABLE IF EXISTS [Message_Types];
+CREATE TABLE [Message_Types]
+(
+    [type_id] INTEGER DEFAULT 1,
+    [type_name] TEXT,
+    CONSTRAINT [PK_type] PRIMARY KEY ([type_id])
+);
+
+DROP TABLE IF EXISTS [Authors];
+CREATE TABLE [Authors]
+(
+    [author_id] INTEGER,
+    [author_name] TEXT,
+    CONSTRAINT [PK_author] PRIMARY KEY ([author_id])
+);
 
 CREATE UNIQUE INDEX [IPK_conv] ON [Conversations]([conv_id]);
 
 CREATE UNIQUE INDEX [IPK_msg] ON [Messages]([msg_id]);
 
+CREATE UNIQUE INDEX [IPK_type] ON [Message_Types]([type_id]);
+
+CREATE UNIQUE INDEX [PPK_author] ON [Authors]([author_id]);
+
 CREATE INDEX [IFK_msg_conv_id] ON [Messages]([msg_conv_id]);
+
+
+INSERT INTO [Message_Types]([type_id], [type_name]) VALUES (1, 'Text');
+INSERT INTO [Message_Types]([type_id], [type_name]) VALUES (2, 'Link');
--- database.py
+++ database.py
@@ -2,22 +2,29 @@ import sqlite3
 
 
 class BastardSQL():
-    def __init__(self):
-        self.conn = sqlite3.connect('bastard.db')
+    def __init__(self, filename='bastard.db'):
+        self.messages_per_page = 20
+        self.changed = False
+        self.conn = sqlite3.connect(filename)
         self.conn.row_factory = sqlite3.Row
         self.cursor = self.conn.cursor()
+
+    def __del__(self):
+        if self.changed:
+            self._commit()
+        self.conn.close()
 
     def _populate(self):
         """Populates bastard.db using bastard.sql"""
         f = open('bastard.sql', 'r')
         sql = f.read()
         self.cursor.executescript(sql)
-        pass
+        f.close()
 
-    def commit(self):
-    	"""Write last operations in the database"""
-    	#FIXME add try-catch?
-    	self.conn.commit()
+    def _commit(self):
+        """Write last operations in the database"""
+        self.conn.commit()
+        self.changed = False
 
     def get_conversations(self):
         """Returns an iterable list of conversations"""
@@ -25,33 +32,64 @@ class BastardSQL():
 
     def get_conversation(self, conv_id):
         """Returns a single conversation if found"""
-        return self.cursor.execute("SELECT * FROM [Conversations] WHERE [conv_id] = ?", conv_id)
+        query = "SELECT * FROM [Conversations] WHERE [conv_id] = ?"
+        result = self.cursor.execute(query, (conv_id,))
+        return result.fetchone()
 
     def put_conversation(self, conv_id, conv_name, participants):
         """Inserts a conversation in the Conversations table, returns the conv_id"""
-        self.cursor.execute("INSERT INTO [Conversations] ([conv_id], [conv_name], [participants]) VALUES (?, ?, ?)", (
-                conv_id,
-                conv_name,
-                participants
-            ))
-        return self.cursor.lastrowid
+        query = "INSERT INTO [Conversations] ([conv_id], [conv_name], [participants]) VALUES (?, ?, ?)"
+        self.cursor.execute(query, (conv_id, conv_name, participants))
+        row_id = self.cursor.lastrowid
+        self.changed = True
+        return row_id
 
-    def get_messages(self, conv_id=0):
-        """Returns an iterable list of all messages (or the messages on a conversation if conv_id is defined)"""
+    def get_messages(self, conv_id=0, msg_type=0, page=0):
+        """Returns an iterable list of messages"""
+        
+        # FIXME: this should be sanitized.
+        conditions = []
         if conv_id > 0:
-            return self.cursor.execute("SELECT * FROM [Messages] WHERE msg_conv_id = {} ORDER BY [msg_timestamp] DESC".format(conv_id))
-        else:
-            return self.cursor.execute("SELECT * FROM [Messages] ORDER BY [msg_timestamp] DESC".format(conv_id))
+            conditions.append("msg_conv_id = {}".format(conv_id))
+        if msg_type > 0:
+            conditions.append("msg_type = {}".format(msg_type))
 
-    def put_message(self, conv_id, message, author, timestamp):
+        where = " WHERE " + " AND ".join(conditions) if conditions else ""
+        
+        if page > 0:
+            page_lbound = page * self.messages_per_page
+            page_ubound = page_lbound + self.messages_per_page
+            limit = " LIMIT {},{}".format(page_lbound, page_ubound)
+        else:
+            limit = ""
+
+        query = "SELECT * FROM [Messages]" + where + " ORDER BY [msg_timestamp] DESC" + limit
+        return self.cursor.execute(query)
+
+    def put_message(self, conv_id, message, author, timestamp, msg_type=1):
         """Inserts a message in the Messages table, returns the row ID"""
-        self.cursor.execute("INSERT INTO [Messages] ([msg_conv_id], [msg_content], [msg_author_id], [msg_timestamp]) VALUES (?, ?, ?, ?)", (
-                conv_id,
-                message,
-                author,
-                timestamp
-            ))
+        query = "INSERT INTO [Messages] ([msg_conv_id], [msg_content], [msg_author_id], [msg_timestamp], [msg_type]) VALUES (?, ?, ?, ?, ?)"
+        self.cursor.execute(query, (conv_id, message, author, timestamp, msg_type))
+        self.changed = True
         return self.cursor.lastrowid
+
+    def get_authors(self):
+        """Returns all the authors"""
+        query = "SELECT * FROM [Authors]"
+        return  self.cursor.execute(query)
+
+    def get_author(self, author_gaia_id):
+        """Returns a single author"""
+        query = "SELECT * FROM [Authors] WHERE [author_id] = ?"
+        result = self.cursor.execute(query, author_gaia_id)
+        return result.fetchone()
+
+    def put_author(self, author_gaia_id, author_name):
+        """Adds an author to the Authors table"""
+        query = "INSERT INTO [Authors] ([author_id], [author_name]) VALUES (?, ?)"
+        self.cursor.execute(query, (author_gaia_id, author_name))
+        self.changed = True 
+        return self.cursor.lastrowid # FIXME will this work if we don't commit right now?
 
     def add_tags(self, msg_id, author, tags):
         """Adds tags to a message"""
