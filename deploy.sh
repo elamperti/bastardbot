95af8e5d3e18a677807899ad48e6b2f629afdef4
Christian Musa <christianmusa@gmail.com>
2015-02-01 21:49:53 -0300

    testing deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-cd $(dirname "$(readlink -fn "$0")")
+echo $PWD
+pyenv local
 screen -ls | grep botsession && screen -x botsession -X stuff "^C"
 screen -ls | grep webserver && screen -x webserver -X stuff "^C"
 pip install -r requirements.txt
