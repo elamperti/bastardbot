748f06389d5a3805d176e469060cf6e7eae4d35f
Christian Musa <christianmusa@gmail.com>
2015-02-03 00:05:54 -0300

    fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-echo $PWD
-echo $CWD
-pyenv local
+export PYENV_VERSION="3.3.4"
+export PATH="$HOME/.pyenv/bin:$HOME/.pyenv/shims:$PATH"
+git pull
 screen -ls | grep botsession && screen -x botsession -X stuff "^C"
 screen -ls | grep webserver && screen -x webserver -X stuff "^C"
 pip install -r requirements.txt
